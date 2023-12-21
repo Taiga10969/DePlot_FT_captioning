03188f529989193851263427b1d6d8519f51bc16
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-12-21 14:20:22 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,20 +1,22 @@
+import os
 import time
 import torch
 import torch.optim as optim
 import torch.nn as nn
+from torchinfo import summary
 
 from models.pix2struct.processing_pix2struct import Pix2StructProcessor
 from models.pix2struct.modeling_pix2struct import Pix2StructForConditionalGeneration
 
 from configs import DePlot_FT_Config
 from datasets import SciCapPlusDataset
-from utils import torch_fix_seed, train_one_epoch, test_one_epoch
+from utils import torch_fix_seed, train_one_epoch, test_one_epoch, freeze_pix2struct_parameters
 
 config = DePlot_FT_Config()
 
 if config.wandb == True:
     import wandb
-    wandb.init(project="train_gpt2v_version2_latest",
+    wandb.init(project="DePlot_FT_captioning",
            name=config.project_name,
            config=config)
 else:
@@ -28,16 +30,25 @@ print("-------------------")
 
 # check GPU usage
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-if torch.cuda.device_count()==0: print('Use 1 GPU')
+if torch.cuda.device_count()==0: print('Use 1 GPU') 
 else: print(f'Use {torch.cuda.device_count()} GPUs')
 
+# check project name
+save_dir = f'./run_result_{config.project_name}'
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+else:
+    print("train [info] : This project name is already running")
+    raise RuntimeError("Duplicate project name. Program stopped.")
 
 
 processor = Pix2StructProcessor.from_pretrained('google/deplot')
 processor.image_processor.is_vqa = False # vqaタスクの設定をvqa=TueからFalseに変更 >> これにより，"decoder_attention_mask", "decoder_input_ids"が追加される．
 
 model = Pix2StructForConditionalGeneration.from_pretrained('google/deplot').to(device)
-model.to(torch.float32)
+print("train [info] : model.dtype : ", model.dtype)
+freeze_pix2struct_parameters(model)
+summary(model)
 
 if config.is_DataParallel == True:
     model = nn.DataParallel(model)
@@ -102,7 +113,7 @@ for epoch in range(1, config.num_eposh + 1, 1):
     end_time = time.time()  # Record the end time of the epoch
     epoch_time = end_time - start_time  # Calculate the time taken for the epoch
     
-    if config.wandb == True:
+    if config.wandb:
         wandb.log({"epoch": epoch,
                    "train_loss": train_loss,
                    "test_loss": test_loss,
@@ -112,8 +123,8 @@ for epoch in range(1, config.num_eposh + 1, 1):
 
     # Save the model every 5 epochs
     if epoch % config.save_param_time == 0:
-        model_save_path = f"./result/{config.project_name}model_epoch_{epoch}.pth"
-        if config.is_DataParallel == True:
+        model_save_path = f"./run_result_{config.project_name}/run_result_{config.project_name}_epoch_{epoch}.pth"
+        if config.is_DataParallel:
             torch.save(model.module.state_dict(), model_save_path)
         else:
             torch.save(model.state_dict(), model_save_path)
